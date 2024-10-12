b57d97f1662543abcecbc0a8cb429c24eadcaff5
PRITHVI RAJ <prithvi@PRITHVIs-Laptop.local>
2024-10-12 11:30:31 +0530

    Initial commit

---
+++ CS py.py
@@ -0,0 +1,28 @@
+
+import streamlit as st
+
+def website():
+    st. set_page_config(page_title="My Webpage", page_icon="check_mark_button", layout="wide")
+    st.title("Attendify")
+    st.subheader ("Say Goodbye to Manual Attendance – Simplify with Attendify!")
+    st.write("Attendify simplifies class attendance management, making it fast, easy, and efficient for educators." )
+    st.write("[INSTALLATION LINK](https://www.appsheet.com/newshortcut/54b26e7c-c99b-40f0-b4b7-8178a3ae966a) ")
+    st.write("[BROWSER LINK](https://www.appsheet.com/start/54b26e7c-c99b-40f0-b4b7-8178a3ae966a) ")
+    st.write("_______________________________________________________________________________________________________________________________________________________________________________________________________")
+    st.write("FOR ANY QUERIES")
+    st.write("PRITHVIRAJ: 8143863455")
+    st.write("SACHIN: 9384474909")
+website()
+
+
+
+
+
+
+
+
+
+
+
+
+
